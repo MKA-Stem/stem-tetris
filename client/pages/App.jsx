92b17c90ea16b319115724aa8994a2f9d616772f
William Goodall <wgoodall01@gmail.com>
2017-04-16 10:24:44 -0700

    Frontend page frame complete

--- client/pages/App.jsx
+++ client/pages/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {Route} from "react-router-dom";
+import {Route, Link} from "react-router-dom";
+import {Helmet} from "react-helmet";
 
 import styles from "pages/App.css";
 
@@ -18,7 +19,28 @@ const about = (props) => (
 )
 
 export default function App(props){
-	return <div>
+	const sep = <span className={styles.navSep}>{String.fromCharCode("8226")}</span>
+	return <div className={styles.page}>
+		<Helmet defaultTitle="STEM Tetris" titleTemplate="%s - STEM Tetris">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			<html lang="en"/>
+			<body className={styles.body}/>
+		</Helmet>
+		<div className={styles.header}>
+			<Link to="/" className={styles.title}>STEM Tetris</Link>
+			<nav className={styles.navLinksContainer}>
+				{[
+					["/",            "Home"],
+					["/about",       "About"],
+					["/leaderboard", "Leaderboard"]
+				].map((e, i, arr) => {
+					const link = <Link to={e[0]} className={styles.navLink}>{e[1]}</Link>;
+					return i==0?<span key={i}>{link}</span>
+						:<span key={i}>{sep}{link}</span>;
+				})}
+			</nav>
+		</div>
+
 		<h1 className={styles.testClass}>This is the app header</h1>
 		<pre>Styles: {JSON.stringify(styles)}</pre>
 		<Route exact path="/" component={main}/>
